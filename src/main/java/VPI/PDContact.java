9b049963083ea09d5439474850cc294f6e4dea04
gebozuhlke <gebo@zuhlke.com>
2016-04-14 11:37:07 +0100

    Creaded classes for PDcontact and Vcontact

---
+++ src/main/java/VPI/PDContact.java
@@ -0,0 +1,94 @@
+package VPI;
+
+/**
+ * Created by gebo on 14/04/2016.
+ */
+public class PDContact {
+    private Long id;
+    private String name;
+    private Long org_id;
+    private ContactDetail[] email;
+    private ContactDetail[] phone;
+    private  Boolean visible_to;
+
+    public PDContact() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getOrg_id() {
+        return org_id;
+    }
+
+    public void setOrg_id(Long org_id) {
+        this.org_id = org_id;
+    }
+
+    public ContactDetail[] getEmail() {
+        return email;
+    }
+
+    public void setEmail(ContactDetail[] email) {
+        this.email = email;
+    }
+
+    public ContactDetail[] getPhone() {
+        return phone;
+    }
+
+    public void setPhone(ContactDetail[] phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getVisible_to() {
+        return visible_to;
+    }
+
+    public void setVisible_to(Boolean visible_to) {
+        this.visible_to = visible_to;
+    }
+
+    @Override
+    public String toString(){
+        return "Number " + id + ":" + name;
+    }
+    //------------------------------------------------------------------ContactDetail
+    class ContactDetail {
+        private String value;
+        private Boolean primary;
+
+        public ContactDetail() {
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public Boolean getPrimary() {
+            return primary;
+        }
+
+        public void setPrimary(Boolean primary) {
+            this.primary = primary;
+        }
+    }
+
+}
---
+++ src/main/java/VPI/VContact.java
@@ -0,0 +1,33 @@
+package VPI;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by gebo on 14/04/2016.
+ */
+public class VContact {
+    @JsonProperty("Id")
+    private Long id;
+
+    @JsonProperty("Full Name")
+    private String name;
+
+    public VContact() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
