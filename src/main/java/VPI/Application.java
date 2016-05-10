3165f891a45b344492514135e5786be7881e870a
sambuddzuhlke <sabu@zuhlke.com>
2016-05-10 12:02:32 +0100

    application tidy up

--- src/main/java/VPI/Application.java
+++ src/main/java/VPI/Application.java
@@ -1,16 +1,12 @@
 package VPI;
 
-import VPI.PDClasses.PDService;
-import VPI.VertecClasses.VertecService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,42 +15,30 @@ import java.util.TimerTask;
 public class Application implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-    private static final String server = "https://api.pipedrive.com/v1/";
 
     public static void main(String args[]) {
         SpringApplication.run(Application.class);
     }
 
-    private PDService PDS;
+    private VertecSynchroniser VS;
 
     @Override
     public void run(String... args) throws Exception {
 
-        MyCredentials creds = new MyCredentials();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        PDS = new PDService(restTemplate, server, creds.getApiKey());
-        VertecService VS = new VertecService("localhost:9999");
+        this.VS = new VertecSynchroniser();
 
         try {
-
-            //TODO: Add code for synchronisation to scheduled task
 
             Timer timer = new Timer();
             TimerTask t = new TimerTask() {
                 @Override
                 public void run() {
-
+                    //Place code to run repeatedly here
                 }
             };
-            //
-            timer.schedule(t, 0l, 1000*60*60);
-            System.out.println(VS.ping());
+            //schedules task to run every hour
+            timer.schedule(t, 0L, 1000*60*60);
 
-
-            //DO STUFF HERE
-            //synchroniser.importOrganisations();
         } catch (Exception e) {
             log.info("HELP HELP IVE HIT AN EXCEPTION" + e.toString());
         }
